4bd4cc2fa69d6b63dc43c2c59a01a74f6ed75d32
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-08 22:44:17 +0100

    Made a new implementation for the game

--- game.py
+++ game.py
@@ -1,21 +1,37 @@
 import pygame
 
+from window import Window
+from gameObjects import load
+
 class Game:
     def __init__(self):
-        self.screen = pygame.display.set_mode((800, 400))
-        self.window_size = pygame.display.get_window_size()
-        
-        self.icon = ''
-        self.caption = ''
-
+        load()
         self.actors = []
+        self.running = True
+        self.window = Window()
 
     def processInput(self):
-        pass
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            
+        for actor in self.actors:
+            actor.processInput(events)
 
     def update(self):
-        pass
+        for actor in self.actors:
+            actor.update()
 
     def draw(self):
-        pass
-    
+        self.window.fill()
+        for actor in self.actors:
+            actor.draw()
+        self.window.update()
+        
+    def isRunning(self):
+        return self.running
+
+    def setRunning(self, value):
+        self.running = value
