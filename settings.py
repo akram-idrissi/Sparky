6183e58dab4e31c96794bca1435b111563f858bd
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-08 23:56:49 +0100

    Added a settings file for common games settings

---
+++ settings.py
@@ -0,0 +1,15 @@
+# window size
+WIDTH = 800
+HEIGHT = 400
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+
+# colors
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+
+# fps
+FPS = 60
