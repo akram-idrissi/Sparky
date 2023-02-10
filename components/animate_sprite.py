401e40cf0efe6064ea589df563b87430fc6f670b
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-10 11:41:38 +0100

    Added a new class for sprite animation

---
+++ components/animate_sprite.py
@@ -0,0 +1,50 @@
+from .sprite import Sprite
+
+class AnimateSprite(Sprite):
+    def __init__(self, actor):
+        super().__init__(actor)
+
+        self.animations = {}
+        self.current_animation = None
+        self.frames = []
+        self.index = 0
+
+    def update(self):
+        self.frames = self.animations[self.current_animation]
+        self.index %= len(self.frames)
+        self.image = self.frames[int(self.index)]
+        self.index += 0.1
+
+    def add_frames(self, name, frames):
+        """ Adds more frames to an animation """
+        if(name and len(frames) > 0):
+            self.animations[name] += frames
+ 
+    def add_animation(self, name, frames):
+        """ Adds a new animation """
+        if(name and len(frames) > 0):
+            self.animations[name] = frames
+            self.current_animation = name
+
+    # getters setters
+    def set_animation(self, name):
+        """ Updates the current animation """
+        if(name): self.current_animation = name 
+
+    def get_animation(self, name):
+        """ Returns the current animation """
+        if name:
+            return self.current_animation
+        return None
+
+    def get_frames(self, name):
+        """ Gets frames of a specific animation """
+        if name and name in self.animations.keys:
+            return self.animations[name]
+        return None
+
+    def set_frames(self, name, frames):
+        """ Sets frames of a specific animation """
+        if(name and name in self.animations.keys):
+            self.animations[name] = frames
+            self.current_animation = name
