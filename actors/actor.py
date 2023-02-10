f84e66427c2441f016aa6fe26db05d88bfb33828
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-10 09:21:53 +0100

    Added new attributes

--- actors/actor.py
+++ actors/actor.py
@@ -1,8 +1,14 @@
+import pygame
+
 class Actor:
     
     def __init__(self, game):
         self.game = game
         self.game.add_actor(self)
+
+        self.position = pygame.math.Vector2()
+        self.velocity = pygame.math.Vector2()
+        self.acceleration = pygame.math.Vector2()
 
         self.components = set()
 
@@ -22,3 +28,26 @@ class Actor:
     
     def process_input(self, events):
         pass
+
+    # getters setters
+    def get_engine(self):
+        return self.game
+
+    def get_position(self):
+        return self.position
+    
+    def set_position(self, x, y):
+        self.position.x = x; self.position.y = y
+    
+    def get_velocity(self):
+        return self.velocity
+    
+    def set_velocity(self, x, y):
+        self.velocity.x = x; self.velocity.y = y
+    
+    def get_acceleration(self):
+        return self.velocity
+    
+    def set_acceleration(self, x, y):
+        self.acceleration.x = x; self.acceleration.y = y
+    
