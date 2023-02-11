c9827088dad5f0bd4dd5ebac164264e388e6f91d
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-11 12:42:22 +0100

    New testing code

--- actors/player.py
+++ actors/player.py
@@ -1,16 +1,19 @@
-from actors.actor import Actor
-from components.animate_sprite import AnimateSprite
+from .actor import Actor
+from components.input import Input
+from components.animSprite import AnimSprite
 
 
 class Player(Actor):
     def __init__(self, game):
         super().__init__(game)
 
+        self.input = None
         self.animation = None
         self.load()
 
     def load(self):
-        self.animation = AnimateSprite(self)
+        self.input = Input(self)
+        self.animation = AnimSprite(self)
         self.animation.add_animation("run", [
             self.animation.load_img('assets/dino/run1.png'),
             self.animation.load_img('assets/dino/run2.png'),
