776597a9415d08aa535434771b4d9415bd7a7ceb
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-11 12:04:47 +0100

    Added class description

--- components/component.py
+++ components/component.py
@@ -1,5 +1,19 @@
 class Component:
     def __init__(self, actor):
+        """
+        Simple base class for components definition
+        
+        The base class for game components. Derived classes will want to
+        override the Component.update() and Component.draw(). 
+        The initializer can accept an Actor().
+
+        When subclassing the Component class, be sure to call the base initializer.
+
+        Attributes:
+            actor: a reference to the owner of this component.
+            engine: a reference to the engine object.
+            window: a reference to the window object.
+        """
         self.actor = actor
         self.actor.add_component(self)
         
