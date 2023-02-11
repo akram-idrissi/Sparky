924c1aaf60087b042fe303503102d1ed7f7c09b0
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-11 12:16:39 +0100

    Updated class and added a description

--- components/sprite.py
+++ components/sprite.py
@@ -2,12 +2,29 @@ import pygame
 from components.component import Component
 
 class Sprite(Component):
+    """
+    Simple base class for visible game objects.
+
+    This class is responsible for rendering and updating all 
+    engine graphics. It renders a single sprite per instance, 
+    for an animated image check AnimateSprite()
+
+    Derived classes can override the Sprite.update() and Sprite.draw() 
+    and assign Sprite.image and Sprite.rect attributes. 
+
+    When subclassing the Sprite class, be sure to call the base initializer.
+
+    Attributes:
+        image: surface to render
+        rect: an image rectangle 
+        engine: a reference to the engine object.
+        screen: a reference to the display surface.
+    """
     def __init__(self, actor):
         super().__init__(actor)
 
         self.rect = None
         self.image = None
-        self.actor = actor
         self.engine.add_sprite(self)
         self.screen = self.window.get_screen()
 
