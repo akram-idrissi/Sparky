07043852d5347f9f13cd61c1a9dd6f309e9aea0b
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-11 11:04:18 +0100

    Added class and methods description

--- actors/actor.py
+++ actors/actor.py
@@ -1,7 +1,18 @@
 import pygame
 
 class Actor:
-    
+    """
+    This is a base Actor class with a handful of overridable methods, each actor also has
+    a set of components.
+
+    Attributes:
+        components: a set that contains unique components objects.
+        engine: a reference to the engine object.
+        window: a reference to the window object.
+        position: a vector that define the x and y coordinates of the actor.
+        velocity: a vector that updates the position each frame.
+        acceleration: a vector that updates the velocity each frame.
+    """
     def __init__(self, engine):
         self.components = set()
         self.engine = engine
@@ -18,14 +29,21 @@ class Actor:
         self.components.add(component) if len(self.components) > 0 else ''
 
     def update(self):
+        """ Updates actor's components and the actor itself """
         for component in self.components:
             component.update()
         self.update_actor()
 
     def update_actor(self):
+        """ An overridable method for updating actor behavior """
         pass
     
     def process_input(self, events):
+        """ 
+        An overridable method that handles actor input
+        Arguments:
+            events: list of events triggered.
+        """
         pass
 
     # getters setters
