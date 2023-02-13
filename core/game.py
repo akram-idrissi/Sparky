cde6bf54390012daa08401c3c7830366b3c0e121
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-13 23:09:44 +0100

    Using the map class

--- core/game.py
+++ core/game.py
@@ -1,6 +1,8 @@
 from .engine import Engine
 from .window import Window
+from actors.map import Map
 from actors.player import Player
+from components.layer import Layer
 
 
 window = Window()
@@ -10,3 +12,6 @@ window.set_caption('Game Engine using pygame')
 engine = Engine(window)
 player = Player(engine)
 player.set_position(100, 100)
+
+map = Map(engine)
+map.add_layer('terrain', Layer(map, 'assets/layers/terrain.csv', 'assets/terrain.png', 64))
