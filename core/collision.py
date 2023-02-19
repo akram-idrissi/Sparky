8e79cf2f366c66d34b53c242953f206ec57ca7fd
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-19 11:58:33 +0100

    Added a new module for collisions testing

---
+++ core/collision.py
@@ -0,0 +1,48 @@
+import pygame
+
+def right_collision(rect1, rect2):
+    return rect1.right >= rect2.left
+
+
+def left_collision(rect1, rect2):
+    return rect1.left <= rect2.right
+
+
+def top_collision(rect1, rect2):
+    return rect1.top <= rect2.bottom
+
+
+def bottom_collision(rect1, rect2):
+    return rect1.bottom >= rect2.top
+
+
+def collidedtiles(rect, tiles):
+    return [tile for tile in tiles if tile.colliderect(rect)]
+
+
+def vertical_movement(rect, velocity, tiles):
+    collisions = {'right': False, 'left': False}
+
+    for tile in tiles:
+        if velocity.x > 0:
+            rect.right = tile.left
+            collisions['right'] = True
+        elif velocity.x < 0:
+            rect.left = tile.right
+            collisions['left'] = True
+    
+    return rect, collisions
+
+
+def horizontal_movement(rect, velocity, tiles):
+    collisions = {'top': False, 'bottom': False}
+
+    for tile in tiles:
+        if velocity.y > 0:
+            rect.bottom = tile.top
+            collisions['right'] = True
+        elif velocity.y < 0:
+            rect.top = tile.bottom
+            collisions['left'] = True
+    
+    return rect, collisions
