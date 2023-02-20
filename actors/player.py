d8b4611036bebb2a0b0ef9efa71f0c7493a9b396
akram-idrissi <akram.mo.gb@gmail.com>
2023-02-20 20:26:19 +0100

    Added jump mecanism

--- actors/player.py
+++ actors/player.py
@@ -7,10 +7,13 @@ class Player(Actor):
     def __init__(self, game):
         super().__init__(game)
 
-        self.speed = 4
+        self.speed = 2
         self.input = None
         self.sprite = None
-
+        self.jump = False
+        self.momentum = 0
+        self.timer = 0
+        self.gravity = 0.8
         self.load()
 
     def load(self):
@@ -24,11 +27,10 @@ class Player(Actor):
             self.velocity.x -= self.speed
         if self.input.keyheld('right'):
             self.velocity.x += self.speed
-        if self.input.keyheld('up'):
-            self.velocity.y -= self.speed
-        if self.input.keyheld('down'):
-            self.velocity.y += self.speed
-
+        if self.input.keydown(events, 'space'):
+            if self.timer < 6:
+                self.acceleration.y = -5
+            
     def collision(self):
         rect = self.sprite.rect
         tiles = self.engine.layers['terrain'].rects
@@ -36,14 +38,22 @@ class Player(Actor):
         self.position.x += self.velocity.x
         rect.x = self.position.x
         collisions = collidedtiles(rect, tiles) 
-        pos, _ = vertical_movement(rect, self.velocity, collisions)
+        pos, _ = horizontal_movement(rect, self.velocity, collisions)
         self.position.x = pos.x
 
         self.position.y += self.velocity.y
         rect.y = self.position.y
         collisions = collidedtiles(rect, tiles) 
-        pos, _ = horizontal_movement(rect, self.velocity, collisions)
+        pos, sides = vertical_movement(rect, self.velocity, collisions)
+        if sides['bottom']:
+            self.acceleration.y = 0
+            self.timer = 0
+        else:
+            self.timer += 1 
         self.position.y = pos.y
 
     def update_actor(self): 
+        super().update_actor()
+        self.acceleration.y += 0.2
+        if self.acceleration.y > 3: self.acceleration.y = 3 
         self.collision()
